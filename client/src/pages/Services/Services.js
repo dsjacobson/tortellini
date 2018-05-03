599da298f6b108ef1095cf8cc021851aa8c7628f
CarL Liu <thecarlliu99@er-10-129-29-154.wireless.umn.edu>
2018-05-02 19:09:13 -0500

    Fixed Services render code format

--- client/src/pages/Services/Services.js
+++ client/src/pages/Services/Services.js
@@ -3,7 +3,7 @@ import "./Services.css";
 
 const services = require("../../services.json");
 
-class Service extends React.Component {
+class Services extends React.Component {
 
 	setService = () => {
 
@@ -22,45 +22,48 @@ class Service extends React.Component {
 			service = services[2];
 			return service;
 		}
-	}
-};
+	};
 
-render() {
-	return(
-		<div className="service-header">
-			<h3 className="service-title">{this.setService().name}</h3>
-			<img src={this.setService().backgroundImage} alt=""/>
-		</div>
+    render() {
+        return(
+            <div>
+                <div className="service-header">
+                    <h3 className="service-title">{this.setService().name}</h3>
+                    <img src={this.setService().backgroundImage} alt=""/>
+                </div>
 
-		<div className="service-content">
-			<p className="">{this.setService().content.sectionContent}</p>
-		</div>
+                <div className="service-content">
+                    <p className="">{this.setService().content.sectionContent}</p>
+                </div>
 
-		<div className="secondary-services">
-			<div className="sec-service-card">
-				<i className={this.setService().secondaryServices.secIconOne}></i>
-				<h4 className="sec-cat-card">{this.setService().secondaryServices.secServiceOne}</h4>
-				<p className="sec-cat-card">{this.setService().secondaryServices.secDescriptionOne}</p>
-			</div>
-			<div className="sec-service-card">
-				<i className={this.setService().secondaryServices.secIconTwo}></i>
-				<h4 className="sec-cat-card">{this.setService().secondaryServices.secServiceTwo}</h4>
-				<p className="sec-cat-card">{this.setService().secondaryServices.secDescriptionTwo}</p>
-			</div>
-			<div className="sec-service-card">
-				<i className={this.setService().secondaryServices.secIconThree}></i>
-				<h4 className="sec-cat-card">{this.setService().secondaryServices.secServiceThree}</h4>
-				<p className="sec-cat-card">{this.setService().secondaryServices.secDescriptionThree}</p>
-			</div>
-		</div>
+                <div className="secondary-services">
+                    <div className="sec-service-card">
+                        <i className={this.setService().secondaryServices.secIconOne}/>
+                        <h4 className="sec-cat-card">{this.setService().secondaryServices.secServiceOne}</h4>
+                        <p className="sec-cat-card">{this.setService().secondaryServices.secDescriptionOne}</p>
+                    </div>
+                </div>
+                <div className="sec-service-card">
+                    <i className={this.setService().secondaryServices.secIconTwo}/>
+                    <h4 className="sec-cat-card">{this.setService().secondaryServices.secServiceTwo}</h4>
+                    <p className="sec-cat-card">{this.setService().secondaryServices.secDescriptionTwo}</p>
+                </div>
+                <div className="sec-service-card">
+                    <i className={this.setService().secondaryServices.secIconThree}/>
+                    <h4 className="sec-cat-card">{this.setService().secondaryServices.secServiceThree}</h4>
+                    <p className="sec-cat-card">{this.setService().secondaryServices.secDescriptionThree}</p>
+                </div>
 
-		<div className="cta-bar">
-			<p className="cta-message">{this.setService().content.ctaMessage}</p>
-			<button className="cta-button">Contact Us</button>
-		</div>
-
-	)
+                <div className="cta-bar">
+                    <p className="cta-message">{this.setService().content.ctaMessage}</p>
+                    <button className="cta-button">Contact Us</button>
+                </div>
+            </div>
+        )
+    }
 }
+
+
 
 
 export default Services;
